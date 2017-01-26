0e6ad232362f011f466a76b809daa4642734056f
Eric Hirshfield-Yamanishi <ehirshfield@gmail.com>
2017-01-26 00:01:33 -0800

    Heroku db setup

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
-  var Burger = sequelize.define("burgers", {
+  var burgers = sequelize.define("burgers", {
     burger_name: DataTypes.STRING,
     devoured: DataTypes.BOOLEAN
   });
-  return Burger;
+  return burgers;
 }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,19 +3,33 @@ var db = require("../models");
  module.exports = function(app){
 
     app.get("/", function(req, res){
-      db.Burger.findAll({}).then(function(allBurgers){
+      db.burgers.findAll({}).then(function(allBurgers){
+
         var hbsObject = {
-          burger: data
+          burger: allBurgers
         };
         console.log(hbsObject);
+
         res.render("index", hbsObject);
       });
     });
 
     app.post("/", function(req, res){
-      db.Burger.create({
+      db.burgers.create({
         burger_name: req.body.name,
         devoured: false
+      }).then(function(){
+        res.redirect("/");
+      });
+    });
+
+    app.put("/:id", function(req, res){
+      db.burgers.update({
+        devoured: true
+      }, {
+        where: {
+          id: req.params.id
+        }
       }).then(function(){
         res.redirect("/");
       });
